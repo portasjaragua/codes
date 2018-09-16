6c65f8db2ca2a0e24703abb2981a4ada58a83b72
Portas Jaraguá <42942616+portasjaragua@users.noreply.github.com>
2018-09-16 09:32:40 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
 if ('serviceWorker' in navigator) {
 	self.addEventListener('fetch', (event) => {});
-  navigator.serviceWorker.register('/sw.js');
+  navigator.serviceWorker.register('https://raw.githubusercontent.com/portasjaragua/codes/master/sw.js');
 }
